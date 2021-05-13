95431b68057626ad30079895d6c1ee3d2abafd8e
Eduardo Galeano <cegard689@gmail.com>
2021-05-12 23:50:36 -0500

    Added list of items.

--- src/components/main/mainComponent.js
+++ src/components/main/mainComponent.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import Home from '../home/homeComponent';
-import Login from '../login/loginComponent';
 import { Router, Switch, Route, Redirect, withRouter, useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { LOGIN_REQUEST, LOGGED_OUT } from '../../actions/actionTypes';
 import { initialState } from '../../reducers/initialState';
+import React from 'react';
+import Home from '../home/homeComponent';
+import Login from '../login/loginComponent';
+import ItemsList from '../itemsList/itemsListComponent';
+import ItemDetail from '../ItemDetail/ItemDetailComponent'
 
 
 const mapStateToProps = (state) => {
+
   return {
     items: state.userRole.items || initialState.items,
     loggedIn: state.userRole.loggedIn || initialState.loggedIn
@@ -26,7 +29,17 @@ const mapDispatchToProps = (dispatch) => {
 
 const Main = (props) => {
     const history = useHistory();
+
     
+    const ItemWithId = ({ match }) => {
+      return (
+        <ItemDetail
+          item={props.items.filter((item) => item.id === parseInt(match.params.itemId))[0]}
+        />
+      );
+    };
+    
+
     return (
       <div>
         <Router history={history} token = {props.loggedIn.token}>
@@ -36,8 +49,10 @@ const Main = (props) => {
               path="/login" 
               component={() => <Login loginFunction = {props.login} loggedStatus = {props.loggedIn}
                   history = {history}
-              />} 
+              />}
             />
+            <Route exact path="/list" component={() => <ItemsList items={props.items} />} />
+            <Route path="/list/:itemId" component={ItemWithId} />
             <Redirect to="/home" />
           </Switch>
         </Router>
